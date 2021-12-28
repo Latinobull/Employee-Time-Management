b253a342482fd537b5a82ae587dd09fa2a5876cf
Donnahue George <Donnahuegjr@gmail.com>
2021-12-28 14:51:01 -0500

    creating users in database

--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config import DATABASE_URI
-from model import Base
+from model import Base, Employee
 
 engine = create_engine(DATABASE_URI)
 Session = sessionmaker(bind=engine)
@@ -12,5 +12,11 @@ def recreate_database():
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
 
+
+employee = Employee(
+    id=1866219,
+    firstName='Donnahue',
+    lastName='George'
+)
 
 recreate_database()
