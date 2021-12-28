d9d23d9708050973f9d29c486ff7f4cee402137b
Donnahue George <Donnahuegjr@gmail.com>
2021-12-28 13:05:04 -0500

    working on front end to get code GUI working correctly

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, ttk
+from tkinter import StringVar, Tk, ttk
 from tkinter.messagebox import showinfo
 
 
@@ -9,15 +9,28 @@ class App(Tk):
         self.title('Employee Login')
         self.geometry('800x500')
 
-        self.label = ttk.Label(self, text='Enter Employee ID')
+        # Main Label
+        self.label = ttk.Label(
+            self, text='Enter Employee ID', font='none 30 bold')
         self.label.pack()
 
-        self.button = ttk.Button(self, text='Submit')
-        self.button['command'] = self.Submit
-        self.button.pack()
+        # ID Entry
+        self.idEntry = StringVar()
+        self.idInput = ttk.Entry(
+            self, textvariable=self.idEntry, font='none 30 normal')
+        self.idInput.focus()
+        self.idInput.pack()
+
+        # Submit to verify user
+        self.submit = ttk.Button(self, text='Submit', width=30)
+        self.submit.bind('<Button-1>', lambda event: self.Submit())
+        self.submit.pack()
+
+        self.bind('<Return>', lambda event: self.Submit())
 
     def Submit(self):
-        showinfo(title='Conformation', message='It was submitted correctly')
+        showinfo(title='Conformation',
+                 message=f'Employee {self.idEntry.get()} was Submitted')
 
 
 if __name__ == '__main__':
