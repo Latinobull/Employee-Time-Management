29d59d33b60c3f3a3504674feeb248c8b7af035e
Donnahue George <Donnahuegjr@gmail.com>
2021-12-28 13:25:28 -0500

    creating models file

---
+++ model.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Date
+
+Base = declarative_base()
+
+
+class Employee(Base):
+    __tablename__ = 'Employee_Time'
+    id = Column(Integer, primary_key=True)
+    firstName = Column(String)
+    lastName = Column(String)
+    startShift = Column(Date)
+    startBreak = Column(Date)
+    endBreak = Column(Date)
+    endShift = Column(Date)
+
+    def __repr__(self) -> str:
+        return "<Employee(firstName='{}',lastName='{}', startShift={}, startBreak={},endBreak={},endShift={})> "\
+            .format(self.firstName, self.lastName, self.startShift, self.startBreak, self.endBreak, self.endShift)
